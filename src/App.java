a1029a07697e156ec99a032fcbc8a64c7f13a35a
Luiz Gustavo de Oliveira <luiz.pf@outlook.com>
2022-07-20 22:09:01 -0300

    Aula 3 (API Nasa, Criacao: ClienteHttp e Extratores)

--- src/App.java
+++ src/App.java
@@ -1,41 +1,34 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        String url = "https://alura-filmes.herokuapp.com/conteudos";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
+        // String url = "https://alura-filmes.herokuapp.com/conteudos";
+        // ExtratorConteudo extrator = new ExtratorConteudoImdb();
 
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body =response.body();
+        String url = "https://api.nasa.gov/planetary/apod?api_key=AaZVCluksUYd1Kzb15BWcSGWqagiIAFk6tf1J3CG&start_date=2022-05-01&end_date=2022-06-01";
+        ExtratorConteudo extrator = new ExtratorConteudoNasa();
 
-        var parser = new JsonParser();
-        var star = new Star();
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
-        for (Map<String,String> filme : listaDeFilmes) {
-            String titulo = filme.get("title");
-            String urlImage = filme.get("image");
-            String nomeArquivo = (titulo + ".png").replaceAll(":", " ");
-            InputStream inputStream = new URL(urlImage).openStream();
+        var geradora = new GeradoraDeFigurinhas();
 
-            var geradora = new GeradoraDeFigurinhas();
+        for (int i = 0 ; i < 3; i ++) {
+            Conteudo conteudo = conteudos.get(i);
+
+            String nomeArquivo = (conteudo.getTitulo() + ".png");
+            InputStream inputStream = new URL(conteudo.getUrlImage()).openStream();
+
+            System.out.println(conteudo.getTitulo());
+            System.out.println(conteudo.getUrlImage());
             geradora.cria(inputStream, nomeArquivo);
-
-            System.out.println(filme.get("title"));
-            System.out.println(star.countStar(filme.get("imDbRating")));   
-        }
+        };
     }
 }
